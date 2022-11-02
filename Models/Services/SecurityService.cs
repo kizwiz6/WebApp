3a079570fefea93d2f7ce7904bed39a1650df841
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-02 13:50:48 +0000

    Handing the user object to another class.

--- Models/Services/SecurityService.cs
+++ Models/Services/SecurityService.cs
@@ -5,18 +5,13 @@
     /// </summary>
     public class SecurityService
     {
-        List<UserModel> knownUsers = new List<UserModel>();
+        UsersDAO usersDAO = new UsersDAO();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SecurityService"/> class.
         /// </summary>
         public SecurityService()
         {
-            this.knownUsers.Add(new UserModel { Id = 0, UserName = "Kieran", Password = "Emery" });
-            this.knownUsers.Add(new UserModel { Id = 1, UserName = "BillGates", Password = "bigbucks" });
-            this.knownUsers.Add(new UserModel { Id = 2, UserName = "MarieCurie", Password = "radioactive" });
-            this.knownUsers.Add(new UserModel { Id = 3, UserName = "WatsonCrick", Password = "dna" });
-            this.knownUsers.Add(new UserModel { Id = 4, UserName = "AlexanderFlemming", Password = "penicillin" });
         }
 
         /// <summary>
@@ -26,9 +21,10 @@
         /// <returns>Boolean return type</returns>
         public bool IsValid(UserModel user)
         {
+            return usersDAO.FindUserByNameAndPassword(user);
+
             // Return true if user is found in the list.
             // For each item x in the list is there a match
-            return this.knownUsers.Any(x => x.UserName == user.UserName && x.Password == user.Password);
         }
     }
 }
